995fcd5e5b27f1b2301f1afdb84fa029ad6283be
JeanEudes-dev <jeudesmails@gmail.com>
2024-04-20 22:24:02 +0300

    "Update Header component with Font Awesome search icon and remove form search element"
    This commit updates the Header component by replacing the form search element with a Font Awesome search icon button. The new button has the same functionality as the previous search

--- frontend/src/component/Header.tsx
+++ frontend/src/component/Header.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import { FaSearch } from "react-icons/fa";
 
 const Header: React.FC = () => {
   const navigate = useNavigate()
@@ -7,10 +8,9 @@ const Header: React.FC = () => {
     <nav className="navbar bg-body-tertiary">
       <div className="container-fluid">
         <span className="navbar-brand">Music Web</span>
-        <form className="d-flex" role="search">
-          <input className="form-control me-2" type="search" onClick={() => navigate('/search')} placeholder="Search" aria-label="Search" />
-          <button className="btn btn-outline-success" type="submit">Search</button>
-        </form>
+        <button className="btn-s" title=' search' type="button">
+          <FaSearch onClick={() => navigate('/search')} />
+        </button>
       </div>
     </nav>
   );
