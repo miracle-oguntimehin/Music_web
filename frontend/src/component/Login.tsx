2e669faba973016044b3acadc465836204349542
miracle-oguntimehin <miracleoguntimehin@gmail.com>
2024-05-21 17:17:41 +0300

    .

--- frontend/src/component/Login.tsx
+++ frontend/src/component/Login.tsx
@@ -1,14 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FaSpotify } from "react-icons/fa";
+import { Modal } from 'antd';
 
 const Login: React.FC = () => {
-  const clientId = '1b744272ce5646098063ccb0ecdfbab0';
+  const [clientId, setClientId] = useState('')
+  const [clientSecret, setClientSecret] = useState('')
+  const [visible, setVisible] = useState(false);
   const redirectUrl = 'http://localhost:3000/';
+
   const handleLoginClick = () => {
+    setVisible(true);
     localStorage.removeItem('id')
+    
+  };
+  const handleLoginSubmit = () => {
     const authUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=code&redirect_uri=${redirectUrl}&scope=streaming%20user-read-private%20user-read-email`;
     window.location.href = authUrl;
-  };
+  }
+
+  const handleIdChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setClientId(e.target.value)
+  }
+
+  const handleSecretChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setClientSecret(e.target.value)
+  }
 
   return (
     <>
@@ -16,6 +32,30 @@ const Login: React.FC = () => {
         Login
         <FaSpotify className='icon' />
       </button>
+
+      {visible && 
+      <Modal
+      title="Login using your spotify dev account"
+      open={visible}
+      onOk={handleLoginSubmit}
+      onCancel={() => setVisible(false)}
+  >
+      <input
+          type="text"
+          placeholder="Please enter your client ID"
+          value={clientId}
+          onChange={handleIdChange}
+          className='search-input'
+      />
+      <input
+          type="text"
+          placeholder="Please enter your client Secret"
+          value={clientSecret}
+          onChange={handleSecretChange}
+          className='search-input'
+      />
+  </Modal>
+      }
 
     </>);
 };
